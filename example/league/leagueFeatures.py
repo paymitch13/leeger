1a0cf083aa98c3088ef4891d5413f77597d3e951
paymitch13 <112431335+paymitch13@users.noreply.github.com>
2022-09-13 03:22:42 -0500

    Update leagueFeatures.py

--- example/league/leagueFeatures.py
+++ example/league/leagueFeatures.py
@@ -1,4 +1,4 @@
-from leeger.league_loader import ESPNLeagueLoader, SleeperLeagueLoader
+from leeger.league_loader import  SleeperLeagueLoader
 from leeger.model.league import League
 
 # Sometimes, you may want to combine League objects.
@@ -7,16 +7,12 @@ from leeger.model.league import League
 
 # Say you had a league that had years 2019-2020 on ESPN and years 2021-2022 on Sleeper and you wanted to create a single League object for those years.
 
-# First, get League from ESPN for 2019-2020.
-espnLeagueLoader = ESPNLeagueLoader("12345678", [2019, 2020])
-espnLeague: League = espnLeagueLoader.loadLeague()
-
 # Second, get League from Sleeper for 2021-2022.
-sleeperLeagueLoader = SleeperLeagueLoader("12345678", [2021, 2022])
+sleeperLeagueLoader = SleeperLeagueLoader("869998755678715904", [2022, 2023])
 sleeperLeague: League = sleeperLeagueLoader.loadLeague()
 
 # Finally, combine the leagues to get a single League object.
-myLeague = espnLeague + sleeperLeague
+myLeague = sleeperLeague
 
 #   Special behaviors:
 #       - name
